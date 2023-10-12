2629f4eeeee1c9eb1e5c6bad47a84945687688e0
Sethuraman Palaniappan <sethuraman_palaniappan@carmax.com>
2023-10-12 15:07:39 -0400

    review 1

--- src/BookshelfChanger.js
+++ src/BookshelfChanger.js
@@ -9,17 +9,31 @@ const BookshelfChanger = ({ book, shelf, onMove }) => {
       onMove(book, selectedValue);
     };
 
+    const options = [
+      { value: 'move', label: 'Move to...', disabled: true },
+      { value: 'currentlyReading', label: 'Currently Reading' },
+      { value: 'wantToRead', label: 'Want to Read' },
+      { value: 'read', label: 'Read' },
+      { value: 'none', label: 'None' },
+    ];
+
+
     return (
       <div className="book-shelf-changer">
         <select value={value} onChange={handleChange}>
-          <option value="move" disabled>
-            Move to...
-          </option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
+         {options.map((option) => (
+              <option key={option.value} value={option.value} disabled={option.disabled}>
+                {option.label}
+              </option>
+          ))}
+        {/* //   <option value="move" disabled>
+        //     Move to...
+        //   </option>
+        //   <option value="currentlyReading">Currently Reading</option>
+        //   <option value="wantToRead">Want to Read</option>
+        //   <option value="read">Read</option>
+        //   <option value="none">None</option>*/}
+         </select>
       </div>
     );
   };
